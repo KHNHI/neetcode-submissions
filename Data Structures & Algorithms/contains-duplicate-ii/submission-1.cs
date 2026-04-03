df2190e20e9c4ee0185516a2767434072f8a0d5c
KHNHI <120494950+KHNHI@users.noreply.github.com>
2026-04-03 21:55:25 +0700

    Add: contains-duplicate-ii - submission-1

---
+++ Data Structures & Algorithms/contains-duplicate-ii/submission-1.cs
@@ -0,0 +1,17 @@
+public class Solution {
+    public bool ContainsNearbyDuplicate(int[] nums, int k) {
+     HashSet<int> window = new HashSet<int>();
+     int l=0;
+     for(int r =0; r<nums.Length; r++){
+        if((r-l)>k){
+            window.Remove(nums[l]);
+            l++;
+        }
+        if(window.Contains(nums[r])){
+            return true;
+        }
+        window.Add(nums[r]);
+     }
+        return false;
+    }
+}
